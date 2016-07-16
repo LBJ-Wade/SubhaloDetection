8e40e4613075155283ab0dfa7a5691a8cdf7ba50
Samuel Witte <witte.sam@gmail.com>
2016-07-15 17:48:17 -0700

    Clarified various options for spatial ext tab

--- TableSpatialExt.py
+++ TableSpatialExt.py
@@ -10,4 +10,7 @@ sys.path.insert(0, os.environ['SUBHALO_MAIN_PATH']+'SubhaloDetection')
 from subhalo import *
 
 
-Table_Spatial_Extension(truncate=True)
+Table_Spatial_Extension(d_low=-3., d_high=1., d_num=80, m_low=np.log10(6.48 * 10**6.), 
+                        m_high=np.log10(2.0 * 10 **9), m_num=60, c_low=np.log10(20.),
+                        c_high=2.4, c_num = 50, alpha=0.16, profile=0, 
+                        truncate=True, arxiv_num=10070438)
