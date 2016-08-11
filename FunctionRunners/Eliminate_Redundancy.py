f1f3a8536f2f135fb1bf9991c5d990194438a1bb
Samuel Witte <witte.sam@gmail.com>
2016-08-11 08:15:12 -0500

    Eliminates duplicate lines in all files for specified directory

---
+++ FunctionRunners/Eliminate_Redundancy.py
@@ -0,0 +1,5 @@
+import os,sys
+sys.path.insert(0, os.environ['SUBHALO_MAIN_PATH']+'/SubhaloDetection')
+from helper import *
+
+eliminate_redundant_lines(os.environ['SUBHALO_MAIN_PATH'] + '/SubhaloDetection/Data/Cross_v_Nobs')
