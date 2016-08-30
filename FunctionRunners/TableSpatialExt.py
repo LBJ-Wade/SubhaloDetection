072cee06d42460ab2028452898d733cea8815fec
Samuel Witte <witte.sam@gmail.com>
2016-08-30 09:31:58 -0500

    Starting edits on table spatil ext

--- FunctionRunners/TableSpatialExt.py
+++ FunctionRunners/TableSpatialExt.py
@@ -9,6 +9,6 @@ import os,sys
 sys.path.insert(0, os.environ['SUBHALO_MAIN_PATH']+'/SubhaloDetection')
 from halo_information import *
 
-table_spatial_extension(profile=0, truncate=False, arxiv_num=13131729,
+table_spatial_extension(profile=0, truncate=True, arxiv_num=13131729,
                         M200=False, d_low=-3., d_high=1., d_num=30, 
-                        m_num=20, c_num = 20)
+                        m_num=20, c_num=20)
--- halo_information.py
+++ halo_information.py
@@ -8,71 +8,81 @@ import numpy as np
 import os
 from subhalo import *
 from helper import *
-from profiles import NFW, Einasto
+from profiles import *
 
 
 def table_spatial_extension(profile=0, truncate=False, arxiv_num=10070438,
                             M200=False, d_low=-3., d_high=1., d_num=30, m_num=20,
-                            c_num=20):
+                            c_num=20, m_low=10.**4., m_high=10**7.):
     """ Tables spatial extension for future use.
 
-        Profile Numbers correspond to [Einasto, NFW] # 0 - 1
+        Profile Numbers correspond to [Einasto, NFW, HW] # 0 - 2
     """
-    Profile_names = ['Einasto', 'NFW']
+
     alpha = 0.16
-    file_name = 'SpatialExtension_' + str(Profile_names[profile]) + '_Truncate_' + \
-                str(truncate) + '_Cparam_' + str(arxiv_num) + '_alpha_' + \
-                str(alpha) + '.dat'
+    file_name = 'SpatialExtension_' + str(Profile_list[profile]) + '.dat'
+
     dir = MAIN_PATH + '/SubhaloDetection/Data/'
 
     if truncate:
-        mass_list = np.logspace(np.log10(3.24 * 10. ** 4. / 0.005),
-                                np.log10(2. * 10. ** 9.), m_num)
+        mass_list = np.logspace(np.log10(m_low / 0.005),
+                                np.log10(m_high / 0.005), m_num)
     else:
-        mass_list = np.logspace(np.log10(3.24 * 10. ** 4.), np.log10(1. * 10. ** 7.), m_num)
+        mass_list = np.logspace(np.log10(m_low), np.log10(m_high), m_num)
     dist_list = np.logspace(d_low, d_high, d_num)
-    c_list = np.logspace(np.log10(2.5), 2.4, c_num)
+    if profile < 2:
+        c_list = np.logspace(np.log10(2.5), 2.4, c_num)
+    if profile == 2:
+        rb_list = np.logspace(-3, np.log10(0.5), 20)
+        gamma_list = np.linspace(0.2, 0.85 + 0.351 / 0.861 - 0.1, 20)
+
     for m in mass_list:
         print 'Subhalo mass: ', m
-        for c in c_list:
-            print '     Concentration parameter: ', c
+        if profile < 2:
+            if not os.path.isfile(dir + file_name):
+                file = open('myfile.dat', 'w+')
+                file.close()
+            for c in c_list:
+                print '     Concentration parameter: ', c
 
-            extension_tab = np.zeros(len(dist_list))
-            if profile == 0:
-                subhalo = Einasto(m, alpha, c, truncate=truncate,
+                extension_tab = np.zeros(len(dist_list))
+                if profile == 0:
+                    subhalo = Einasto(m, alpha, c, truncate=truncate,
+                                      arxiv_num=arxiv_num, M200=M200)
+                elif profile == 1:
+                    subhalo = NFW(m, alpha, c, truncate=truncate,
                                   arxiv_num=arxiv_num, M200=M200)
-            elif profile == 1:
-                subhalo = NFW(m, alpha, c, truncate=truncate,
-                              arxiv_num=arxiv_num, M200=M200)
-            else:
-                'Profile not implemented yet'
+                else:
+                    'Profile not implemented yet'
 
-            for ind, d in enumerate(dist_list):
-                print '         Distance', d
-                try:
-                    look_array = np.loadtxt(dir + file_name)
-                    if any((np.round([m, c, d], 5) == x).all() for x in np.round(look_array[:, 0:3], 5)):
-                        exists = True
-                    else:
-                        exists = False
-                except:
-                    exists = False
+                for ind, d in enumerate(dist_list):
+                    print '         Distance', d
 
-                if not exists and subhalo.Full_Extension(d) > .05:
-                    extension_tab[ind] = subhalo.Spatial_Extension(d)
+                    with open(dir + file_name, 'a+') as f:
+                        if not any(value == x.rstrip('\r\n') for x in f):
+                            if subhalo.Full_Extension(d) > 1:
+                                ext = subhalo.Spatial_Extension(d)
+                                value += '      {:.2f}'.format(ext)
+                                f.write(value + '\n')
 
-            extension_tab = extension_tab[np.nonzero(extension_tab)]
-            assert isinstance(extension_tab, object)
-            entries_added = extension_tab.size
-            full_tab = np.vstack((np.ones(entries_added) * m,
-                                  np.ones(entries_added) * c,
-                                  dist_list[:entries_added],
-                                  extension_tab)).transpose()
+        else:
+            if not os.path.isfile(dir + file_name):
+                file = open('myfile.dat', 'w+')
+                file.close()
+            for rb in rb_list:
+                print '     Rb: ', rb
+                for gam in gamma_list:
+                    print '         Gamma: ', gam
+                    extension_tab = np.zeros(len(dist_list))
+                    subhalo = HW_Fit(m, gam=gam, rb=rb, M200=True, gcd=8.5, stiff_rb=False)
+                    for ind, d in enumerate(dist_list):
+                        print '           Distance', d
+                        value = ['{:.4e}'.format(m), '{:.3e}'.format(rb), '{:2f}'.format(gam), '{:.3f}'.format(d)]
+                        with open(dir + file_name, 'a+') as f:
+                            if not any(value == x.rstrip('\r\n') for x in f):
+                                if subhalo.Full_Extension(d) > 1:
+                                    ext = subhalo.Spatial_Extension(d)
+                                    value.append('{:.2f}'.format(ext))
+                                    f.write(value + '\n')
 
-            if os.path.isfile(dir + file_name):
-                load_info = np.loadtxt(dir + file_name)
-                add_to_table = np.vstack((load_info, full_tab))
-                np.savetxt(dir + file_name, add_to_table)
-            else:
-                np.savetxt(dir + file_name, full_tab)
     return
