85074529cc5ec612997a37ca6220bb1028736e19
Samuel Witte <witte.sam@gmail.com>
2016-09-12 12:00:16 -0500

    Tried moving file

--- runs_dmax/Extended_Runner.sh
+++ runs_dmax/Extended_Runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COUNTER=1
 while [  $COUNTER -lt 31 ]; do
-    qsub runs_dmax/Calc_Dmax_commandrunner_$COUNTER.sh
+    qsub Calc_Dmax_commandrunner_$COUNTER.sh
     let COUNTER=COUNTER+1
 done
